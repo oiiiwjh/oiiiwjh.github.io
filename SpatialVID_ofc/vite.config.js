f6d7dbc7b2980d52ce36afbe989cb70bc96677d6
oiiiwjh <2820130752@qq.com>
2025-09-02 23:40:29 +0800

    init spatialVID_ofc

--- SpatialVID_ofc/vite.config.js
+++ SpatialVID_ofc/vite.config.js
@@ -1,11 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 将 base 放到顶层，它会同时应用于开发服务器和构建输出
+  base: '/SpatialVID_ofc/',
   server: {
-    base: '/SpatialVID_ofc/',
+    // 这里就不需要 base 了，因为它已经被顶层的 base 覆盖
     host: true,
     port: 3000
   },
