33601d3573dded813cb6c9323dea0758cb95bc82
oiiiwjh <2820130752@qq.com>
2025-09-03 21:03:45 +0800

    change

--- SpatialVID_ofc/vite.config.js
+++ SpatialVID_ofc/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SpatialVID_ofc/',
   server: {
     host: true,
     port: 3000
