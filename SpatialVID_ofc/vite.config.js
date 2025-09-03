66e2e890cea102cc919ae57dddfbed959619b3b8
oiiiwjh <2820130752@qq.com>
2025-09-03 15:05:35 +0800

    init spatialVID_ofc

--- SpatialVID_ofc/vite.config.js
+++ SpatialVID_ofc/vite.config.js
@@ -1,16 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 3000
-  },
-  base: '/SpatialVID_ofc/', // 确保构建时资源路径正确
-  build: {
-    outDir: 'dist', // 默认就是dist，可以省略
   },
   optimizeDeps: {
     include: ['three', 'gsap']
